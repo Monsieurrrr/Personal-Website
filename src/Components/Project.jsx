4593464f524a4279ef071f52aedd8ec87eb66045
Monsieurrrr <ronnorbert29@gmail.com>
2024-10-17 14:45:44 +0700

    update

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -8,6 +8,7 @@ import thriftit from '../assets/MockUp/thriftit.png'
 import clminton from '../assets/MockUp/clminton.png'
 import skintastic from '../assets/MockUp/skintastic.png'
 import notes from '../assets/MockUp/notes.png'
+import permatamebel from '../assets/MockUp/permatamebel.png'
 import {motion} from 'framer-motion'
 
 
@@ -22,41 +23,48 @@ export default function Project() {
         button:"Website"
       },{
         id: 2,
+        photo: permatamebel,
+        name: "Permata Mebel",
+        desc: "React.js",
+        link: "https://permata-mebel-store.vercel.app/",
+        button:"Website"
+      },,{
+        id: 3,
         photo: dewataIsland,
         name: "Dewata Island",
         desc: "HTML - CSS - JS",
         link: "https://dewata-island.vercel.app/",
         button:"Website"
       },{
-        id: 3,
+        id: 4,
         photo: wheelhub,
         name: "WheelHub",
         desc: "HTML - CSS - JS",
         link: "https://wheel-hub-beta.vercel.app/",
         button:"Website"
       },{
-        id: 4,
+        id: 5,
         photo: clminton,
         name: "CLminton",
         desc: "JAVA - SQL",
         link: "https://github.com/Monsieurrrr/CLminton-javaFX",
         button:"Github"
       },{
-        id: 5,
+        id: 6,
         photo: thriftit,
         name: "ThriftIt",
         desc: "Figma",
         link: "https://www.figma.com/proto/8mFwQipX5hGTYOPZlUeBIo/ThriftIt?node-id=1-5&node-type=canvas&t=Wp5Srjm8waOaxHYz-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A3",
         button:"Prototype"
       },{
-        id: 6,
+        id: 7,
         photo: skintastic,
         name: "Skintastic",
         desc: "Figma",
         link: "https://www.figma.com/proto/tuRrInWVNnz9n5DIIkStku/Skintastic?node-id=37-618&node-type=canvas&t=YmSpqUqySVpJFcND-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A5&show-proto-sidebar=1",
         button:"Prototype"
       },{
-        id: 7,
+        id: 8,
         photo: notes,
         name: "NOTES",
         desc: "Figma",
